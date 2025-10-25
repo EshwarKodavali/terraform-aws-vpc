b0e30cc882dcb67e2d052b7375bb9f2f60c1ec66
unknown <eshwarkodavali123@gmail.com>
2025-10-25 12:13:59 +0530

    terraform7

--- terraform-aws-vpc/peering.tf
+++ terraform-aws-vpc/peering.tf
@@ -23,7 +23,7 @@ resource "aws_vpc_peering_connection" "default" {
 
 resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.subnet_cidr_public.id
+  route_table_id            = aws_route_table.public_route_table.id
   destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
